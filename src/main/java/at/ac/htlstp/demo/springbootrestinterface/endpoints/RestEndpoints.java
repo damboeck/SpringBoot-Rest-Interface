187a443c82ce57553326e838d8a4568fa21b8cb7
Werner <werner.damboeck@letto.at>
2023-02-27 14:26:04 +0100

    RestClient Test

--- src/main/java/at/ac/htlstp/demo/springbootrestinterface/endpoints/RestEndpoints.java
+++ src/main/java/at/ac/htlstp/demo/springbootrestinterface/endpoints/RestEndpoints.java
@@ -2,6 +2,7 @@ package at.ac.htlstp.demo.springbootrestinterface.endpoints;
 
 public class RestEndpoints {
 
+    public static final int ServerPort=22001;
     public static final String BASE = "";
     public static final String OPEN = BASE+"/open";
     public static final String ping = OPEN+"/ping";
