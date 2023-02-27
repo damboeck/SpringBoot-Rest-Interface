ce7c2b9e55242cc0cf88849b834b40420ade88a0
Werner <werner.damboeck@letto.at>
2023-02-27 09:03:53 +0100

    Initial commit

---
+++ src/main/java/at/ac/htlstp/demo/springbootrestinterface/SpringBootRestInterfaceApplication.java
@@ -0,0 +1,13 @@
+package at.ac.htlstp.demo.springbootrestinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestInterfaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/ac/htlstp/demo/springbootrestinterface/SpringBootRestInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ac.htlstp.demo.springbootrestinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
